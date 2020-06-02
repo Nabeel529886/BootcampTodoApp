2192975dea850012fe916a31768c892d5eb5e6d8
Nabeel529886 <48175387+Nabeel529886@users.noreply.github.com>
2020-06-03 00:57:05 +0500

    counter app completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+    state = {
+      counter: 0
+    }
+
+  addCount = () => {
+    this.setState(prevState => ({counter: prevState.counter+1}))
+    console.log(this.state.counter)
+
+  }
+
+  removeCount = () => {
+    this.setState(prevState => ({counter: prevState.counter-1}))
+  }
+
   render(){
     return(
       <div>
-        <h1>Hello, World!</h1>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.addCount}>Add Count</button>
+        <button onClick={this.removeCount}>Remove Count</button>
       </div>
     )
   }
